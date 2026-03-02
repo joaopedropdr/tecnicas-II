a002a60e5187109c3c2fa18ca4b3a33d5e139bc0
João Pedro Pascuci De Russi <jpderussi@gmail.com>
2026-03-02 11:07:11 -0300

    Aula-02-03

--- Heranca/obj/Debug/net9.0/Heranca.AssemblyInfo.cs
+++ Heranca/obj/Debug/net9.0/Heranca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2435057903a322da12ad43369180b5d06ef3d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heranca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
